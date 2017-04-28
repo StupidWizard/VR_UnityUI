7b8cdedcf3e6c1af2adcc9fd3f43cb9e079e9fef
toi-nguyen <toi-nguyen@KYCNT1408-08.local>
2017-04-28 18:39:12 +0900

    Demo event active UIPointer, event click button

--- vr_ui/VR_UI/Assets/VRTKSub/VRTK_ControllerEvents.cs
+++ vr_ui/VR_UI/Assets/VRTKSub/VRTK_ControllerEvents.cs
@@ -13,22 +13,51 @@ namespace VRTKSub {
 			TouchPad
 		}
 
+		public bool[] buttonPressed = new bool[] {false, false, false};
+
 		// Use this for initialization
 //		void Start () {
 //
 //		}
 
 		// Update is called once per frame
-//		void Update () {
-//
-//		}
+		void Update () {
+			if (Input.GetKeyDown(KeyCode.T)) {
+				if (IsButtonPressed(ButtonAlias.TouchPad)) {
+					Release(ButtonAlias.TouchPad);
+				} else {
+					Press(ButtonAlias.TouchPad);
+				}
+			}
+			if (Input.GetKeyDown(KeyCode.P)) {
+				StartCoroutine(ClickTrigger());
+			}
+		}
+
+
+		IEnumerator ClickTrigger () {
+			Press(ButtonAlias.Trigger);
+			yield return null;
+			yield return new WaitForSeconds(0.2f);
+			Release(ButtonAlias.Trigger);
+		}
+
+
 
 		public bool IsButtonPressed(ButtonAlias button) {
-			return true;
+			return buttonPressed[(int)button];
 		}
 
 		public Vector2 GetTouchpadAxis() {
 			return Vector2.zero;
+		}
+
+		void Press(ButtonAlias button) {
+			buttonPressed[(int)button] = true;
+		}
+
+		void Release(ButtonAlias button) {
+			buttonPressed[(int)button] = false;
 		}
 	}
 
